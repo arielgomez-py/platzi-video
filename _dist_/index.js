b3ad6c6a44b7b3fc01979afa2ba52d97eedd33aa
arielgomez-py <arielgomez-py@users.noreply.github.com>
2022-05-24 02:13:36 +0000

    deploy: cc1f68bd13fa5a0bed6e71b362c5bd681e251450

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,1 +1,28 @@
+//Player video
+
+const selecVideo = document.querySelector('video');
+
+// Selected elements
+const playButton = document.getElementById("play-btn");
+const pauseButton = document.getElementById("pause-btn");
+const mutedButton = document.getElementById("mute-btn");
+
+//Properties: play(), pause(), mute()
+const playVideo = () => {
+    
+}
+
+const pauseVideo = () => {
+
+}
+
+const muteVideo = () => {
+    
+}
+
+//Actions or events of DOM for videos
+
+
+//selecVideo.play();
+//selecVideo.pause();
 
