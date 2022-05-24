cc1f68bd13fa5a0bed6e71b362c5bd681e251450
Ariel <arielgomezmendez94@gmail.com>
2022-05-23 22:11:10 -0400

    Select buttons and declaring functions

--- src/index.js
+++ src/index.js
@@ -2,4 +2,27 @@
 
 const selecVideo = document.querySelector('video');
 
+// Selected elements
+const playButton = document.getElementById("play-btn");
+const pauseButton = document.getElementById("pause-btn");
+const mutedButton = document.getElementById("mute-btn");
+
+//Properties: play(), pause(), mute()
+const playVideo = () => {
+    
+}
+
+const pauseVideo = () => {
+
+}
+
+const muteVideo = () => {
+    
+}
+
+//Actions or events of DOM for videos
+
+
+//selecVideo.play();
+//selecVideo.pause();
 
