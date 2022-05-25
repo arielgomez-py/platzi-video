fe263a46a7e69f82142726c263f9114f68145ce0
arielgomez-py <arielgomez-py@users.noreply.github.com>
2022-05-25 17:38:57 +0000

    deploy: 79bef87f7290e4ad449c274116a62aad28c60e40

--- _dist_/index.js
+++ _dist_/index.js
@@ -8,27 +8,30 @@ const pauseButton = document.getElementById("pause-btn");
 const mutedButton = document.getElementById("mute-btn");
 
 //Properties: play(), pause(), mute()
+// Functions
 const playVideo = () => {
-    
+    selecVideo.play();
 }
 
 const pauseVideo = () => {
-
+    selecVideo.pause();
 }
 
 const muteVideo = () => {
+    if(selecVideo.muted === false){
+        selecVideo.muted = true;
+    }
+    else{
+        selecVideo.muted = false;  
+    }
     
 }
 
-<<<<<<< HEAD
 
-=======
-//Actions or events of DOM for videos
 //Actions or events of DOM for videos
 playButton.addEventListener("click", playVideo);
 pauseButton.addEventListener("click", pauseVideo);
 mutedButton.addEventListener("click", muteVideo);
->>>>>>> functions
 
 //selecVideo.play();
 //selecVideo.pause();
