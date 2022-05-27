f60ff9d83e87f2f3f209460b30c7bd9789204216
Ariel <arielgomezmendez94@gmail.com>
2022-05-26 23:08:05 -0400

    Operation of volume up button

--- src/index.js
+++ src/index.js
@@ -6,18 +6,22 @@ const selecVideo = document.querySelector('video');
 const playButton = document.getElementById("play-btn");
 const pauseButton = document.getElementById("pause-btn");
 const mutedButton = document.getElementById("mute-btn");
+const volumePlusButton = document.getElementById("vol-btn-plus");
+const volumeLessButton = document.getElementById("vol-btn-less");
 
 
 //Properties: play(), pause(), mute()
 // Functions
+//Play
 const playVideo = () => {
     selecVideo.play();
 }
 
+//Pause
 const pauseVideo = () => {
     selecVideo.pause();
 }
-
+//Mute
 const muteVideo = () => {
     if(selecVideo.muted === false){
         selecVideo.muted = true;
@@ -26,12 +30,18 @@ const muteVideo = () => {
         selecVideo.muted = false;  
     }    
 }
+// Volume plus
+const volumePlusVideo = () => {
+    alert(selecVideo.volume);
+}
+ //Volume less
 
 
 //Actions or events of DOM for videos
 playButton.addEventListener("click", playVideo);
 pauseButton.addEventListener("click", pauseVideo);
 mutedButton.addEventListener("click", muteVideo);
+volumePlusButton.addEventListener("click", volumePlusVideo);
 
 
 //selecVideo.play();
