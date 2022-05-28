967077e71538e8384135a6601f9d9da3b753fde3
Ariel <arielgomezmendez94@gmail.com>
2022-05-28 09:28:03 -0400

    Resolve conflict

--- src/index.js
+++ src/index.js
@@ -6,13 +6,9 @@ const selecVideo = document.querySelector('video');
 const playButton = document.getElementById("play-btn");
 const pauseButton = document.getElementById("pause-btn");
 const mutedButton = document.getElementById("mute-btn");
-<<<<<<< HEAD
 const volumePlusButton = document.getElementById("vol-btn-plus");
 const volumeLessButton = document.getElementById("vol-btn-less");
 
-=======
-const volumeButton = document.getElementById("volume-btn");
->>>>>>> ad9ad2b005da1224a7aec4f6bb4aa80610216418
 
 //Properties: play(), pause(), mute()
 // Functions
