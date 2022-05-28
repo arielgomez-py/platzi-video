68483ec423a82667aa06e2ae5b6e6e5d892541e0
arielgomez-py <arielgomez-py@users.noreply.github.com>
2022-05-28 13:27:41 +0000

    deploy: 5e4ed33b4d0ee3b74c99951630dd20afaf33831d

--- _dist_/index.js
+++ _dist_/index.js
@@ -6,19 +6,27 @@ const selecVideo = document.querySelector('video');
 const playButton = document.getElementById("play-btn");
 const pauseButton = document.getElementById("pause-btn");
 const mutedButton = document.getElementById("mute-btn");
+<<<<<<< HEAD
+const volumePlusButton = document.getElementById("vol-btn-plus");
+const volumeLessButton = document.getElementById("vol-btn-less");
+
+=======
 const volumeButton = document.getElementById("volume-btn");
+>>>>>>> ad9ad2b005da1224a7aec4f6bb4aa80610216418
 
 //Properties: play(), pause(), mute()
 // Functions
+//Play
 const playVideo = () => {
     selecVideo.play();
 }
 
+//Pause
 const pauseVideo = () => {
     selecVideo.pause();
     
 }
-
+//Mute
 const muteVideo = () => {
     if(selecVideo.muted === false){
         selecVideo.muted = true;
@@ -27,17 +35,20 @@ const muteVideo = () => {
         selecVideo.muted = false;  
     } 
 }
-
-const volumeVideo = () => {
+// Volume plus
+const volumePlusVideo = () => {
     alert(selecVideo.volume);
 }
+ //Volume less
+
 
 
 //Actions or events of DOM for videos
 playButton.addEventListener("click", playVideo);
 pauseButton.addEventListener("click", pauseVideo);
 mutedButton.addEventListener("click", muteVideo);
-volumeButton.addEventListener("click", volumeVideo);
+volumePlusButton.addEventListener("click", volumePlusVideo);
+
 
 //selecVideo.play();
 //selecVideo.pause();
